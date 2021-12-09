7341b04ca985e89d591cdc4d764d205a63d96974
-=EL=- <IT0671581471@gmail.com>
2021-12-09 06:18:34 +0200

    15 creating components in folders

---
+++ src/components/Profile/MyPost/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from './MyPosts.module.css';
+
+const MyPosts = () => {
+    return (
+            <div>
+                <div>My posts</div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+                <div>Post 3</div>
+                <div>Post 4</div>
+            </div>
+    );
+}
+
+export default MyPosts;
