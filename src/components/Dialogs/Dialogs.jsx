600538d36f34677fe0868f7f63311471d7f49704
-=EL=- <IT0671581471@gmail.com>
2022-01-07 23:57:50 +0200

    23 props for page Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,30 +1,40 @@
 import React from "react";
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogsItem = (props) => {
+    const path = "/dialogs/" + props.id;
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+const Message = (props) => {
+    return (
+        <div className={s.massage}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                    <div className={s.dialog + ' ' + s.active}>
-                           Dimich
-                    </div>
-                    <div className={s.dialog}>
-                        Viktor
-                    </div>
-                    <div className={s.dialog}>
-                        Sveta
-                    </div>
-                    <div className={s.dialog}>
-                        Roman
-                    </div>
+                <DialogsItem name="Dimych" id='1'/>
+                <DialogsItem name="Viktor" id='2'/>
+                <DialogsItem name="Sveta" id='4'/>
+                <DialogsItem name="Max" id='5'/>
+                <DialogsItem name="Lora" id='6'/>
+                <DialogsItem name="Gleb" id='7'/>
             </div>
+
             <div className={s.massages}>
-                    <div className={s.massage}>Hi!</div>
-                    <div className={s.massage}>How are you?</div>
-                    <div className={s.massage}>Super!!!</div>
+                <Message message='Hi!'/>
+                <Message message='How are you?'/>
+                <Message message='Super!!!'/>
             </div>
         </div>
     )
 }
 
- export default Dialogs;
+export default Dialogs;
