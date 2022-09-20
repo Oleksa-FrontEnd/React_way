09efe81c902d9f99206d2d57b466109eb6e25011
Oleksa <it0671581471@gmail.com>
2022-09-20 15:59:55 +0300

    37 to wrap up state in store

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,6 +1,6 @@
-import {renderEntireTree} from "../render";
 
-let state = {
+let store = {
+    _state: {
         profilePage: {
             posts: [
                 {id: 1, message: "Hou are you?", estimate: 7},
@@ -8,29 +8,29 @@ let state = {
                 {id: 3, message: "This is my second post", estimate: 37}
             ],
             newPostText: "it-kamasutra.com"
-    },
+        },
         dialogsPage : {
-                dialogs : [
-                    {id: '1', name: 'Dimych', avatar: './../images/54.png'},
-                    {id: '2', name: 'Viktor', avatar: './../images/bober.png'},
-                    {id: '3', name: 'Sveta', avatar: './../images/54.png'},
-                    {id: '4', name: 'Max', avatar: ''},
-                    {id: '5', name: 'Lora', avatar: ''},
-                    {id: '6', name: 'Gleb', avatar: ''}
-                ],
-                messages : [
-                    {id: '1', message: 'Hi!'},
-                    {id: '2', message: 'How are you?'},
-                    {id: '3', message: 'Super!'}
-                ],
-                avatar : [
-                    {id: '1', ava: 'https://i0.wp.com/pbs.twimg.com/media/Dh6Q-IOU0AIX6xl.jpg?w=730'},
-                    {id: '2', ava: './../images/54.png'},
-                    {id: '3', ava: ""},
-                    {id: '4', ava: ""},
-                    {id: '5', ava: ""},
-                    {id: '6', ava: ""},
-                ]
+            dialogs : [
+                {id: '1', name: 'Dimych', avatar: './../images/54.png'},
+                {id: '2', name: 'Viktor', avatar: './../images/bober.png'},
+                {id: '3', name: 'Sveta', avatar: './../images/54.png'},
+                {id: '4', name: 'Max', avatar: ''},
+                {id: '5', name: 'Lora', avatar: ''},
+                {id: '6', name: 'Gleb', avatar: ''}
+            ],
+            messages : [
+                {id: '1', message: 'Hi!'},
+                {id: '2', message: 'How are you?'},
+                {id: '3', message: 'Super!'}
+            ],
+            avatar : [
+                {id: '1', ava: 'https://i0.wp.com/pbs.twimg.com/media/Dh6Q-IOU0AIX6xl.jpg?w=730'},
+                {id: '2', ava: './../images/54.png'},
+                {id: '3', ava: ""},
+                {id: '4', ava: ""},
+                {id: '5', ava: ""},
+                {id: '6', ava: ""},
+            ]
         },
         sidebar : {
             favoritFriends: [
@@ -39,22 +39,30 @@ let state = {
                 {id:3, name: "Omega", avatar: ""}
             ]
         }
+    },
+    getStore(){
+        return this._state
+    },
+    _callSubscriber() {
+        console.log('State changed')},
+    addPost () {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            estimate: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = ('');
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
+    }
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        estimate: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = ('');
-    renderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
-
-export default state;
+export default store;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from "./Redux/state.js";
-import {renderEntireTree} from "./render"
+import store from "./Redux/state.js";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
 
-renderEntireTree(state);
+ let renderEntireTree =(state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={store.getStore()}
+                     addPost={store.addPost.bind(store)}
+                     updateNewPostText={store.updateNewPostText.bind(store)} />
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+ renderEntireTree(store.getStore());
+ store.subscribe (renderEntireTree)
 
 reportWebVitals();
